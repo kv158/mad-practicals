748bc74e2b80d75f8956c509b431900ebca93a9c
kv <krushalvataliya24@gmail.com>
2022-10-02 08:42:03 +0530

    change in handler activity

--- app/src/main/java/com/example/kv2/handlere.java
+++ app/src/main/java/com/example/kv2/handlere.java
@@ -1,45 +1,65 @@
 package com.example.kv2;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.icu.text.DecimalFormat;
+import android.icu.text.NumberFormat;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.widget.TextView;
 
 public class handlere extends AppCompatActivity {
+    int j;
     TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        tv=findViewById(R.id.sec);
-        setContentView(R.layout.activity_handlere);
 
-//        new CountDownTimer(7000, 1000) {
-//            @Override
-//            public void onTick(long millisUntilFinished) {
-//                int j=5;
-//                tv.setText("kwdnkw");
+        setContentView(R.layout.activity_handlere);
+        tv=(TextView) findViewById(R.id.seco);
+                        tv.setText("dfusbdfhweb");
+
+//        j=5;
+
+
+        new CountDownTimer(6000, 1000) {
+
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onTick(long millisUntilFinished) {
+                NumberFormat f=new DecimalFormat("00");
+                long sec =(millisUntilFinished/1000)%60;
+
+
+                tv.setText(f.format(sec));
 //
 //                j--;
-//            }
-//
-//            @Override
-//            public void onFinish() {
-//
-//            }
-//        }.start();
+            }
 
-
-        Handler h = new Handler();
-        h.postDelayed(new Runnable() {
             @Override
-
-            public void run() {
+            public void onFinish() {
+                tv.setText("00");
                 Intent i = new Intent(getApplicationContext(),MainActivity.class);
                 startActivity(i);
 
-            } },5000);
+            }
+        }.start();
+
+
+
+
+//        Handler h = new Handler();
+//        h.postDelayed(new Runnable() {
+//            @Override
+//
+//            public void run() {
+//                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+//                startActivity(i);
+//
+//            } },5000);
     }
 }
